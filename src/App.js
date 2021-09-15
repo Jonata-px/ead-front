0a83bf1e73f6737db99872823a3a62e12c6bbd83
jfcoder <jonatafelixc@gmail.com>
2021-09-14 21:42:50 -0300

    inicio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './views/Header';
+import Footer from './views/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header className="App-header"/>
+        <h2>Conteudo do site</h2>
+      <Footer/>
     </div>
   );
 }
